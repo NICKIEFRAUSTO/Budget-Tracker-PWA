4d13b7fafe2546db642dc0a00de5a7d72bcfdb77
NICKIEFRAUSTO <79732055+NICKIEFRAUSTO@users.noreply.github.com>
2021-05-10 22:43:05 -0500

    updated service worker due to errors not able to cache offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,61 +1,28 @@
-const FILES_TO_CACHE = [
-    './',
-    './index.html',
-    './styles.css',
-    './icons/icon-192x192.png',
-    './icons/icon-512x512.png',
-   ]
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
-// --------------------------install-----------------------------------//
-self.addEventListener("install", (event) =>{
-    // pre cache image data
+// ------------install-------------------------------
+  self.addEventListener('install', event => {
     event.waitUntil(
-      caches
-      .open(PRECACHE)
-      .then((cache) => cache.addAll(FILES_TO_CACHE))
-      .then(self.skipwaiting())
+      caches.open('static').then( cache => {
+        return cache.addAll([
+    "./",
+    "./index.html",
+    "./styles.css",
+    "./db.js",
+    "./manifest.json",
+    "./index.js",
+    "./icons/icon-192x192.png",
+    "./icons/icon-512x512.png",
+   ]);
+})
     );
-    }); 
-    
-  // ------------------------------activate---------------------------//
-  self.addEventListener('activate', (event) => {
-    const currentCaches = [PRECACHE, RUNTIME];
-    event.waitUntil(
-      caches
-        .keys()
-        .then((cacheNames) => {
-          return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-        })
-        .then((cachesToDelete) => {
-          return Promise.all(
-            cachesToDelete.map((cacheToDelete) => {
-              return caches.delete(cacheToDelete);
-            })
-          );
-        })
-        .then(() => self.clients.claim())
-    );
-  });
-//   ----------------------------fetch-----------------------------------//
-self.addEventListener('fetch', (event) => {
-    if (event.request.url.startsWith(self.location.origin)) {
-      event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-          if (cachedResponse) {
-            return cachedResponse;
-          }
-  
-          return caches.open(RUNTIME).then((cache) => {
-            return fetch(event.request).then((response) => {
-              return cache.put(event.request, response.clone()).then(() => {
-                return response;
-              });
-            });
-          });
-        })
-      );
-    }
-  });
-  
-  
+    console.log('Install');
+  self.skipWaiting();
+});
+
+// -----------------retrieve assets from cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then( response => {
+      return response || fetch(event.request);
+    })
+  );
+});
